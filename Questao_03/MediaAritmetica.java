c8d8202f77f2d95bcad4b836c8da9ed05eec1fc0
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-09-26 15:29:07 -0300

    Questao 03

---
+++ Questao_03/MediaAritmetica.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MediaAritmetica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" --- Digite as suas notas --- ");
+
+        System.out.println("Primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+        
+        System.out.println("Sua média é: " + media);
+
+        scanner.close();
+    }
+}
