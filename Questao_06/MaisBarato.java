c32887d2148f0d0f73dad89b84bf993db69eaa27
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-10-02 23:29:36 -0300

    Update MaisBarato.java

--- Questao_06/MaisBarato.java
+++ Questao_06/MaisBarato.java
@@ -5,6 +5,21 @@ public class MaisBarato {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.println("Digite o preço do produto 1: ");
+            double produto1 = scanner.nextDouble();
+            
+            System.out.println("Digite o preço do produto 2: ");
+            double produto2 = scanner.nextDouble();
+            
+            System.out.println("Digite o preço do produto 3: ");
+            double produto3 = scanner.nextDouble();
+
+            if(produto1 <= produto2 && produto1 <= produto3) {
+                System.out.println("O mais barato é o produto 1.");
+            } else if(produto2 <= produto1 && produto2 <= produto3) {
+                System.out.println("O mais barato é o produto 2.");
+            } else { 
+                System.out.println("O mais barato é o produto 3.");
+            }
         }
     }    
 }
